a337c1a1b7e625dc0d712fff8b44f0445146d8c1
Raden Rafi <raden.rafi.personal@gmail.com>
2024-06-24 15:12:35 +0700

    adding user profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,21 +12,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
+        $user = \App\Models\User::factory()->create([
             'name' => 'Administrator',
             'email' => 'admin@admin.com',
             'password' => bcrypt('admin123'),
         ]);
-        // make a family altar without factory
+        \App\Models\Profile::create([
+            'user_id' => 1,
+            'phone' => '08123456789',
+            'gender' => 'Laki-laki',
+            'nij' => '1234567890',
+            'address' => 'Jl. Pahlawan No. 1',
+        ]);
+
         \App\Models\FamilyAltar::create([
             'name' => 'Family Altar 1',
             'address' => 'Address 1',
             'leader_start_date' => '2024-06-15',
             'user_id' => 1,
         ]);
-        // import diakonia seeder
-        $this->call(DiakoniaSeeder::class);
+
+        $this->call([
+            DiakoniaSeeder::class,
+            RoleSeeder::class,
+        ]);
+
+        $user->roles()->sync([1, 2, 3, 4]);
     }
 }
